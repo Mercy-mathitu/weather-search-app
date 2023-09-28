0c96e531bf750b20c26e32897ed4c661ad3a94a9
Mercy M. Wairimu <110403633+Mercy-mathitu@users.noreply.github.com>
2023-09-28 07:01:28 +0300

    added github link

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
       <h1>Weather Search Engine</h1>
       <Search />
-      <a href='#'>Weather-react Github</a>
+      <a href='https://github.com/Mercy-mathitu/weather-search-app'>Weather-react Github</a>
     </div>
     </>
   )
